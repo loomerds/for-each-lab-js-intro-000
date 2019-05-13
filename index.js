b8d7d41ca97116513986697337f7b14c35ade516
Doug Loomer <loomerds@gmail.com>
2019-05-13 11:41:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function iterativeLog(arr) {
-  arr.forEach((element, index) =>{
+function iterativeLog(callback) {
+  callback.forEach((element, index) =>{
     console.log(`${index}: ${element}`);
   });
+}
+
+function iterate(callback) {
+  let arr = [42, 73, 63];
+  arr.forEach(callback);
 }
